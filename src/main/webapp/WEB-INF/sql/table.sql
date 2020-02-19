4165965608b90c939d3f21c68693f11693dfddfd
Administrator <Administrator@JKS155>
2020-02-19 21:15:29 +0900

    zhaktr

---
+++ src/main/webapp/WEB-INF/sql/table.sql
@@ -0,0 +1,18 @@
+# 1. 사용자 테이블	 TB_USR
+# 이름, 비번, 아이디, 생성일자(DATE), 수정일자(DATE)
+# 2. 게시판	TB_BRD
+# 파일명, 글번호(INT), 글제목, 글내용(CLOB), 생성일자(DATE), 수정일자(DATE)
+
+# TB(테이블) + _ + 테이블약어3글자
+# 메타 or 메타스트림 or 메타시스템 or 메타서비스 ->  만약에없다 -> DB사전 or 용어사전 있어야 -> 이거마저 없다.쓰레기
+# int cdNo = request.getparam(as'da'sd')
+#
+
+
+CREATE TABLE TB_USR (
+
+)
+
+CREATE TABLE TB_BRD (
+
+)
